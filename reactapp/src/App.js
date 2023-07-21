8691f80b80a3bfe285bdc29492881c66241aac38
jajuiscool <jacobjuarez1996@gmail.com>
2023-07-20 22:52:05 -0500

    Added UseState
    Added UseState and made api request

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Header, Footer } from './components/common'
 
 import './App.css';
 
 function App() {
+
+  const[backendData,setBackendData] = useState([{}])
+
+  useEffect(() =>{
+      fetch("https://localhost:7085/api/amusement-parks").then(
+          response => response.json()
+      ).then(
+          data => {
+              setBackendData(data)
+          }
+      ) 
+  },[])
+
   return (
     <div className="App">
         <Header />
         <br />
         <br />
-       
+        <p>Does dis work how about this</p>
         <br />
         <br />
         <Footer />
