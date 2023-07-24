b052f6ddcbc7ff2211f6fd8874cc0da1e8dec461
MetalBox47 <35822953+MetalBox47@users.noreply.github.com>
2023-07-24 17:17:42 -0500

    Beginning of Guests Controllers

---
+++ webapi/Controllers/GuestsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webapi.Models;
+using webapi.Services.Interfaces;
+
+namespace webapi.Controllers
+{
+
+    [ApiController]
+    [Route("api/guests")]
+    public class GuestsController : ControllerBase
+    {
+
+        private readonly IGuestService _guestsService;
+
+        public GuestsController(IGuestService guestsService)
+        {
+            _guestsService = guestsService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            List<Guests> guests = await _guestsService.GetAllAsync();
+            return Ok(guests);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetById(int id)
+        {
+            Guests? guest = await _guestsService.GetByIdAsync(id);
+            if (guest != null)
+            { 
+                return Ok(guest);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(UsersCreateDto dto)
+        {
+            if (ModelState.IsValid)
+            {
+                UsersGetDto? user = await _guestsService.GetByIdAsync(dto);
+            }
+        }
+
+    }
+}
---
+++ webapi/Services/Implementations/GuestsService.cs
@@ -0,0 +1,24 @@
+﻿using webapi.Models;
+using webapi.Services.Interfaces;
+
+namespace webapi.Services.Implementations
+{
+    public class GuestsService : IGuestService
+    {
+        private readonly ILogger<GuestsService> _logger;
+
+        public GuestsService(ILogger<GuestsService> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<List<Guests>> GetAllAsync()
+        {
+
+            List<Guests> guests = new();
+
+            throw new NotImplementedException();
+
+        }
+    }
+}
