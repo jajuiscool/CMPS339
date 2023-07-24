b27648480b04a552b64f151f2a363543f0fcfd90
MetalBox47 <35822953+MetalBox47@users.noreply.github.com>
2023-07-24 15:27:58 -0500

    Tickets Controller

---
+++ webapi/Controllers/TicketsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webapi.Models;
+using webapi.Services.Interfaces;
+
+namespace webapi.Controllers
+{
+
+    [ApiController]
+    [Route("api/tickets")]
+    public class TicketsController : ControllerBase
+    {
+
+        private readonly ITicketsService _ticketService;
+
+        public TicketsController(ITicketsService ticketsService)
+        {
+            _ticketService = ticketsService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            List<Tickets> tickets = await _ticketService.GetAllAsync();
+            return Ok(tickets);
+        }
+
+        [HttpGet("tickets")]
+        public async Task<ActionResult> GetTicketsById(int id)
+        {
+            Tickets? ticket = await _ticketService.GetTicket(id);
+            if (ticket != null)
+            {
+                return Ok(ticket);
+            }
+            return NotFound();
+
+        }
+    }
+}
---
+++ webapi/Models/Tickets.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapi.Models
+{
+    [Table("Tickets")]
+    public class Tickets
+    {
+
+        public int Id { get; set; }
+        public int AttractionId { get; set; }
+
+    }
+
+    public class TicketsGetDto
+    {
+        public int Id { get; set; }
+        public int AttractionId { get; set;}
+
+    }
+
+    public class TicketsCreateDto
+    {
+        [Required]
+        public int AttractionId { get; set; }
+    }
+
+}
